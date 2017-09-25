e3b1d119099c77ae345fbae30a5983339b4dcfdd
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 15:59:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,4 @@
+
 def square_array(array)
   new_array = Array.new
   array.each do |number|
@@ -5,3 +6,11 @@ def square_array(array)
   end
   new_array
 end
+
+=begin
+def square_array(array)
+  array.collect do |number|
+    number ** 2
+  end
+end
+=end
